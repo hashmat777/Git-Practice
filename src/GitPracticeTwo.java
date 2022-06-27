7caf5d2e8caedd4bb85c187a47f12c7ef6e7001b
hashmat777 <hashmatazizi87@gmail.com>
2022-06-26 23:30:51 -0700

    update class

---
+++ src/GitPracticeTwo.java
@@ -0,0 +1,14 @@
+
+public class GitPracticeTwo {
+	
+	public int sum (int a, int b) {
+		int sum = a+b;
+		return sum;
+	}
+	
+	public int mult(int a, int b) {
+		int mult = a*b;
+		return mult;
+	}
+
+}
